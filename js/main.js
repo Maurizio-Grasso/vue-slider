663f5e6ce6af8b6252cb7a4019e9cfead8f511f7
maurizio <maurizio.grasso@gmail.com>
2021-03-21 18:15:09 +0100

    ResponsiveMobileFirst

--- js/main.js
+++ js/main.js
@@ -16,6 +16,16 @@ var app = new Vue ({
             } ,
 
             {
+                src : 'scot-3.jpg' , 
+                alt : 'Neist Point, Isola di Skye, Scozia'
+            } ,
+
+            {
+                src : 'scot-4.jpg' , 
+                alt : 'Callanish Standing Stones (Isola di Lewis and Harris)'
+            } ,
+
+            {
                 src : 'sic-1.jpg' , 
                 alt : 'Etna vista da Taormina'
             } ,
@@ -31,39 +41,57 @@ var app = new Vue ({
             } ,
 
             {
-                src : 'tusc-3.jpg',
-                alt : 'Pontremoli, Toscana'
+                src : 'sic-4.jpg' , 
+                alt : 'Gangi (Sicilia)'
+            } ,
+
+            {
+                src : 'sic-5.jpg' , 
+                alt : 'Chiesa Madre di Modica'
+            } ,
+
+            {
+                src : 'sic-6.jpg', 
+                alt : 'Saline di Trapani (Sicilia)'
+            } ,
+
+            {
+                src : 'tusc-1.jpg',
+                alt : 'Ponte del Diavolo di Borgo a Mozzano'
+            } ,
+
+            {
+                src : 'tusc-2.jpg',
+                alt : 'Panoramica di San Gimignano (Siena)'
+            } ,
+
+            {
+                src : 'tusc-3.jpg' ,
+                alt : 'Centro storico di Pontremoli (Toscana)'
             } ,
 
             {
                 src : 'tusc-4.jpg',
-                alt : 'Panorama di Siena (Toscana)'
+                alt : 'Panorama di Siena'
             } ,
 
             {
                 src : 'tusc-5.jpg' ,
                 alt : 'Duomo di Firenze visto da Piazzale Michelangelo'
             }
+
         ]
     } ,
 
     created : function() {
-        // setInterval( () => { this.imgNext()}, 3000);
+       setInterval( () => {this.setImage(this.imgCounter + 1)} , 3000);
     } ,
 
     methods : {
-        imgNext : function() {
-            if (this.imgCounter == this.imgList.length - 1) this.imgCounter = 0;
-            else this.imgCounter++;
-        } ,
-
-        imgPrev : function() {
-            if (this.imgCounter == 0 ) this.imgCounter = this.imgList.length - 1;
-            else this.imgCounter--;
-        } ,
-
         setImage : function(index) {
-            this.imgCounter = index ;
+            if(index < 0)   this.imgCounter = this.imgList.length - 1 ;
+            else if (index >= this.imgList.length) this.imgCounter = 0 ;            
+            else this.imgCounter = index ;
         }
     }
 });
